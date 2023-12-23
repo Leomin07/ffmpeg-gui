1616bbb24a3d15722a6d2d200b9a6dc50ce3d181
MinhTD <tranminhsvp@gmail.com>
2023-12-23 13:55:01 +0700

    optimize code

--- main.py
+++ main.py
@@ -1,98 +1,71 @@
-#!/usr/bin/env python
+import subprocess
+
 import PySimpleGUI as sg
-import cv2 as cv
-
-# from PIL import Image
-# import io
-
-"""
-    Demo program to open and play a file using OpenCV
-    It's main purpose is to show you:
-    1. How to get a frame at a time from a video file using OpenCV
-    2. How to display an image in a PySimpleGUI Window
-    
-    For added fun, you can reposition the video using the slider.
-    
-    Copyright 2022 PySimpleGUI
-"""
 
 
 def main():
-    # ---===--- Get the filename --- #
-    filename = sg.popup_get_file("Filename to play")
-    if filename is None:
-        return
-    vidFile = cv.VideoCapture(filename)
-    # ---===--- Get some Stats --- #
-    num_frames = vidFile.get(cv.CAP_PROP_FRAME_COUNT)
-    fps = vidFile.get(cv.CAP_PROP_FPS)
-
-    sg.theme("Black")
-
-    # ---===--- define the window layout --- #
     layout = [
-        [sg.Text("OpenCV Demo", size=(15, 1), font="Helvetica 20")],
-        [sg.Image(key="-IMAGE-")],
-        [
-            sg.Slider(
-                range=(0, num_frames), size=(60, 10), orientation="h", key="-SLIDER-"
-            )
-        ],
-        [sg.Push(), sg.Button("Exit", font="Helvetica 14")],
-        [sg.Button("Pause", font="Helvetica 14", key="p")],
+        [sg.Button("Browse")],
+        [sg.Text("", size=(40, 1), key="-OUTPUT-")],
+        [sg.Text("Begin", size=(15, 1), font="Helvetica 20")],
+        [sg.Input(key="-BEGIN-")],
+        [sg.Text("End", size=(15, 1), font="Helvetica 20")],
+        [sg.Input(key="-END-")],
+        [sg.Button("Cancel")],
     ]
 
-    # create the window and show it without the plot
     window = sg.Window(
-        "Demo Application - OpenCV Integration",
+        "FFmpeg simple",
         layout,
         no_titlebar=False,
         location=(0, 0),
     )
 
-    # locate the elements we'll be updating. Does the search only 1 time
-    image_elem = window["-IMAGE-"]
-    slider_elem = window["-SLIDER-"]
-    timeout = 800 // fps  # time in ms to use for window reads
+    while True:
+        # print(f"filename {filename}")
+        event, values = window.read()
+        # # See if user wants to quit or window was closed
+        if event == "Cancel" or event is None:
+            break 
 
-    # ---===--- LOOP through video file by frame --- #
-    cur_frame = 0
-    while vidFile.isOpened():
-        event, values = window.read(timeout=timeout)
-        if event in ("Exit", None):
-            break
-        ret, frame = vidFile.read()
-        if not ret:  # if out of data stop looping
-            break
-        # if someone moved the slider manually, the jump to that frame
-        if int(values["-SLIDER-"]) != cur_frame - 1:
-            cur_frame = int(values["-SLIDER-"])
-            print(f"cur_frame {cur_frame}")
-            vidFile.set(cv.CAP_PROP_POS_FRAMES, cur_frame)
-        slider_elem.update(cur_frame)
-        cur_frame += 1
+        if event == "Browse":
+            file = sg.popup_get_file("", no_window=True)
+            if file == "":
+                continue
+            window["-OUTPUT-"].update(file)
+            value_input_begin = values["-BEGIN-"]
+            value_input_end = values["-END-"]
+            new_filename = ""
 
-        imgbytes = cv.imencode(".ppm", frame)[
-            1
-        ].tobytes()  # can also use png.  ppm found to be more efficient
-        image_elem.update(data=imgbytes)
+            type_video = file.split(".")[1:2]
+            if str(type_video[0]) == "ts":
+                new_filename = ".".join(file.split(".")[0:-1]) + ".mp4"
+            else:
+                new_filename = ".".join(file.split(".")[0:-1]) + "_new.mp4"
+
+            subprocess.run(
+                [
+                    "ffmpeg",
+                    "-i",
+                    file,
+                    "-ss",
+                    value_input_begin,
+                    "-to",
+                    value_input_end,
+                    "-c:v",
+                    "copy",
+                    "-c:a",
+                    "copy",
+                    new_filename,
+                ]
+            )
+
+            break
+
+    window.close()
 
 
 main()
 
-#############
-#    | |    #
-#    | |    #
-#    |_|    #
-#  __   __  #
-#  \ \ / /  #
-#   \ V /   #
-#    \_/    #
-"""         #############
-        # This was another way updates were being done, but seems slower than the above
-        img = Image.fromarray(frame)    # create PIL image from frame
-        bio = io.BytesIO()              # a binary memory resident stream
-        img.save(bio, format= 'PNG')    # save image as png to it
-        imgbytes = bio.getvalue()       # this can be used by OpenCV hopefully
-        image_elem.update(data=imgbytes)
-"""
+
+# ffmpeg -i DVDES-513.ts -ss 0:35:40 -to 1:06:45 -c:v copy -c:a copy trimmedVideo.mp4
